7f95fbf1e76958971c9dc46d804d413203e049fa
awais144506 <awaisrasheed68@gmail.com>
2023-09-02 04:39:25 +0500

    Student added

--- Appwrite.js
+++ Appwrite.js
@@ -1,10 +1,7 @@
 import { Client,Account } from "appwrite";
-
-
 const client = new Client();
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('64e346356ddeac62c8ca');
-
-    
+    .setProject('64e346356ddeac62c8ca')
 export const account = new Account(client)
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Text} from 'react-native';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuth } from '../api/auth/AuthProvider';
+import { account } from '../Appwrite'; // Import the correct module for fetching user data
 
 const HomeScreen = () => {
   const { user } = useAuth();
-  const [username, setUsername] = useState('');
+  const [studentName, setStudentName] = useState('');
+  const [rollNO, setRollNo] = useState('');
 
   useEffect(() => {
     if (user) {
-      const userUsername = user.name;
-      setUsername(userUsername);
+      account
+        .get(user.$id) 
+        .then(function (userData) {
+          const userName = userData.name;
+          const userRollNo = userData.rollNO;
+          setStudentName(userName);
+          setRollNo(userRollNo);
+        })
+        .catch(function (error) {
+          console.log('Error fetching user data:', error);
+        });
     }
   }, [user]);
 
   return (
     <SafeAreaView>
-      <Text style={{ textAlign: 'center', marginTop: 5 }}>Welcome {username}</Text>
+      <Text style={{ textAlign: 'center', marginTop: 5 }}>Welcome {studentName}</Text>
+      <Text style={{ textAlign: 'center', marginTop: 5 }}>Roll No: {rollNO}</Text>
     </SafeAreaView>
   );
 };
